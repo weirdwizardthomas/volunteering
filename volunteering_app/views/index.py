365ddde32efc064957e59a37a3c70a4c57a3fdf2
Tomas Koristka <koristo1@fit.cvut.cz>
2021-06-19 16:55:47 +0200

    Added modelviewset based API.
    (cherry picked from commit b55982f6069e563c5bdc5ff0e2e4d4229fa288e0)

--- volunteering_app/views/index.py
+++ volunteering_app/views/index.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 
 
 class Index(TemplateView):
-    template_name = 'volunteering_app/index.html'
+    ...
---
+++ volunteering_app/views/volunteers_view_set.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from volunteering_app.models import Volunteer
+from volunteering_app.serialisers.volunteer_serialiser import VolunteerSerialiser
+
+
+class VolunteersViewSet(viewsets.ModelViewSet):
+    queryset = Volunteer.objects.all()
+    serializer_class = VolunteerSerialiser
+
+    def _get_object(self):
+        id = self.kwargs['pk']
+        return self.get_queryset().get(id=id)
+
+    def list(self, request, **kwargs):
+        serialiser = self.serializer_class(self.queryset, many=True)
+        return Response(serialiser.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self._get_object()
+        serialiser = self.serializer_class(instance)
+        return Response(serialiser.data)
